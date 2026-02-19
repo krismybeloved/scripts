a9c69f3ec1584cdbbaeae6bdf5cc2110574f0c2f
bumblebee <los-sanchxz-sk1929801-ksla90221@proton.me>
2026-02-19 22:38:04 +0300

    Implement fling and ESP testing in mm2.lua
    Add functionality for fling and ESP testing.

---
+++ mm2.lua
@@ -0,0 +1,6 @@
+-- fling
+pcall(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hellohellohell012321/KAWAII-FREAKY-FLING/main/kawaii_freaky_fling.lua",true))()
+end)
+
+-- esp (testing the script)
