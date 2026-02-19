8df5034e943532931b13c3fc188e42973ef082cc
bumblebee <los-sanchxz-sk1929801-ksla90221@proton.me>
2026-02-19 22:49:23 +0300

    upd

--- mm2.lua
+++ mm2.lua
@@ -1,6 +1,17 @@
--- fling
+-- [Fling]
 pcall(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hellohellohell012321/KAWAII-FREAKY-FLING/main/kawaii_freaky_fling.lua",true))()
 end)
 
--- esp (testing the script)
+
+-- [Esp]
+local success, result = pcall(function()
+    return loadstring(game:HttpGet("https://pastebin.com/raw/jW9Btku2"))()
+end)
+
+if not success then
+    warn("[Error :  Esp] Failed to load script: " .. tostring(result))
+end
+
+-- [Small Note]
+print(" [Message :  Dev] This script made for destroying teamers. ")
